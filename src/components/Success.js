e3517f7378b606e6e25245e5c3343c4359ca4fed
SiddhantBhardwaj2018 <bhardwajsiddhant03@gmail.com>
2020-10-24 12:40:02 -0700

    first commit

---
+++ src/components/Success.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import {List, ListItem} from 'material-ui/List'
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class Success extends Component {
+
+    continue = e => {
+        e.preventDefault();
+        // PROCESS FORM //
+        this.props.nextStep();
+    }
+
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Success" />
+                        <h1>Thank You For Your Submission ! </h1>
+                        <p>You will get an email with further instructions !</p>
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+
+
+export default Success;
